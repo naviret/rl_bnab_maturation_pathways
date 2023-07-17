f85243803955439ff1c3b5491753693cabc10af9
naviret <111366578+naviret@users.noreply.github.com>
2023-07-17 09:22:32 -0400

    data pipepline setup

---
+++ development/proteomic_translation_pipeline/data_scripts/mixcr_pipeline.sh
@@ -0,0 +1,10 @@
+mixcr analyze generic-amplicon \
+    --species hs \
+    --rna \
+    --floating-left-alignment-boundary \
+    --floating-right-alignment-boundary J \
+    --drop-non-CDR3-alignments \
+    --
+    SRR8235277_R1.fastq \
+    SRR8235277_R2.fastq \
+	results \ 
